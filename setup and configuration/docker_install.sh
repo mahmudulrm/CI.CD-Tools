7aa505ab769f1dc7be3ea66d9b58eddfa4145756
mahmudulrm <mahmudulrm@gmail.com>
2020-11-28 18:26:19 +0600

    docker install script

---
+++ setup and configuration/docker_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install docker from Docker-ce repository
+echo "[TASK 2] Install docker container engine"
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum install -y -q docker-ce >/dev/null 2>&1
+
+# Enable docker service
+echo "[TASK 3] Enable and start docker service"
+systemctl enable docker >/dev/null 2>&1
+systemctl start docker
+
+usermod -aG docker ansadmin
