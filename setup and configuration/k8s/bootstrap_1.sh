8e278264bcd061a48785ae42fd24944798a26dd2
mahmudulrm <mahmudulrm@gmail.com>
2020-11-28 18:27:57 +0600

    k8s script

---
+++ setup and configuration/k8s/bootstrap_1.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Install docker from Docker-ce repository
+echo "[TASK 2] Install docker container engine"
+
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum install -y -q docker-ce >/dev/null 2>&1
+
+# Enable docker service
+echo "[TASK 3] Enable and start docker service"
+systemctl enable docker >/dev/null 2>&1
+systemctl start docker
+
+# Disable SELinux
+echo "[TASK 4] Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+# Stop and disable firewalld
+echo "[TASK 5] Stop and Disable firewalld"
+systemctl disable firewalld >/dev/null 2>&1
+systemctl stop firewalld
+
+# Add sysctl settings
+echo "[TASK 6] Add sysctl settings"
+cat >> /etc/sysctl.conf<<EOF
+net.ipv4.ip_forward=1
+net.bridge.bridge-nf-call-iptables=1
+net.ipv4.neigh.default.gc_thresh1=4096
+net.ipv4.neigh.default.gc_thresh2=6144
+net.ipv4.neigh.default.gc_thresh3=8192
+EOF
+
+modprobe br_netfilter
+sysctl -p
+
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+vm.swappiness=0
+EOF
+
+sysctl --system >/dev/null 2>&1
+
+# Disable swap
+echo "[TASK 7] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Add yum repo file for Kubernetes
+echo "[TASK 8] Add yum repo file for kubernetes"
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Install Kubernetes
+echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
+yum install -y -q kubeadm kubelet kubectl >/dev/null 2>&1
+
+# Start and Enable kubelet service
+echo "[TASK 10] Enable and start kubelet service"
+systemctl enable kubelet >/dev/null 2>&1
+systemctl start kubelet >/dev/null 2>&1
